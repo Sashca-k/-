47fd3226fadd431ce2bab8eaf9cf45a24caeda50
Sashca-k <62010729+Sashca-k@users.noreply.github.com>
2020-04-11 10:53:53 +0300

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO [Products] (Name, Price)VALUES(N'Ручка', 60);
+
+UPDATE [Products] SET [Price]=90 WHERE [Name]=N'Ручка';
+
+SELECT COUNT(*) AS Count FROM [Products]
+
+DELETE FROM [Products] WHERE [Price]>1000;
